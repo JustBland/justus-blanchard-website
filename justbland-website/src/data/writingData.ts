ab885bd0595d4becd91eaaff8021b61b5ab3cc30
JustBland <Justbland8@gmail.com>
2026-09-25 10:46:06 -0500

    Edited slug

--- justbland-website/src/data/writingData.ts
+++ justbland-website/src/data/writingData.ts
@@ -1,6 +1,6 @@
 export default [
     {
-        slug: "How-to-computer",
+        slug: "how-to-computer",
         title: 'How to Use a Computer',
         date: 'September 22nd, 2026',
         snippet: `words words words words words words words words words 
